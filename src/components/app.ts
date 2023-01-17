0141bd7dbc78d97929ca396da540f9714494f62a
RaidasE30 <raidasb@gmail.com>
2023-01-17 17:09:43 +0200

    feat: initial app component

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,15 +1,28 @@
+import CarsCollection from '../helpers/cars-collection';
+import cars from '../data/cars';
+import brands from '../data/brands';
+import models from '../data/models';
+
 class App {
   private htmlElement: HTMLElement;
 
-  constructor(selector: string) {
+  private carsCollection: CarsCollection;
+
+  public constructor(selector: string) {
     const foundElement = document.querySelector<HTMLElement>(selector);
 
     if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
 
     this.htmlElement = foundElement;
+    this.carsCollection = new CarsCollection({
+      cars,
+      brands,
+      models,
+    });
+    console.log(this.carsCollection);
   }
 
-  initialize = (): void => {
+  public initialize = (): void => {
     const container = document.createElement('div');
     container.className = 'container my-5';
     container.innerHTML = 'Laukiu kol būsiu sukurtas';
